fad102e7edf31c1f713c0e1b5e004d898e9f212f
Gooodofwaar <Askhat.Maratov@list.ru>
2023-11-21 12:23:05 +0600

    Checking data

--- src/main/java/com/example/ymeqpvuunrfubnfukxtd/service/implementation/model_service_impl/CallOneServiceImpl.java
+++ src/main/java/com/example/ymeqpvuunrfubnfukxtd/service/implementation/model_service_impl/CallOneServiceImpl.java
@@ -2,9 +2,19 @@ package com.example.ymeqpvuunrfubnfukxtd.service.implementation.model_service_im
 
 import com.example.ymeqpvuunrfubnfukxtd.model.entity.CallOne;
 import com.example.ymeqpvuunrfubnfukxtd.model.service_dto.CallOneDTO;
+import com.example.ymeqpvuunrfubnfukxtd.repository.CallOneRepository;
 import com.example.ymeqpvuunrfubnfukxtd.service.model_service.CallOneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+@Service
+@RequiredArgsConstructor
 public class CallOneServiceImpl implements CallOneService {
+    private final CallOneRepository callOneRepository;
     public CallOne callOneDTOToCallOne(CallOneDTO callOneDTO) {
         CallOne callOne = new CallOne();
         callOne.setName(callOneDTO.getName());
@@ -25,16 +35,45 @@ public class CallOneServiceImpl implements CallOneService {
         return callOneDTO;
     }
 
-    public Boolean checkPhoneNumber() {
-        return true;
+    // Длина номера телефона 11-12 символов (может + быть)
+    public Boolean checkPhoneNumber(String number) {
+        String regex = "(^[78]\\d{10}$)|(^\\+[78]\\d{10}$)";
+        return number.matches(regex);
     }
 
-    public void addPhoneNumber() {
-
+    public Boolean checkName(String name) {
+        String nameRegex = "^[A-Za-z\\s'-]+$";
+        return name.matches(nameRegex);
     }
 
-    public String getPhoneNumber() {
-        return "";
+    public Boolean checkYear(Integer year) {
+        return year >= 1900 && year <= 2023;
+    }
+
+    // Day/Month/Year from 01.01.1900 to now
+    public Boolean checkDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        try {
+            LocalDate parsedDate = LocalDate.parse(date, formatter);
+            LocalDate startDate = LocalDate.of(1900, 1, 1);
+            LocalDate currentDate = LocalDate.now();
+            return (parsedDate.isEqual(startDate) || parsedDate.isAfter(startDate)) &&
+                    (parsedDate.isEqual(currentDate) || parsedDate.isBefore(currentDate));
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public void addPhoneObject(CallOne callOne) {
+        callOneRepository.save(callOne);
+    }
+
+    public String getCallObjectById(Long id) {
+        if (callOneRepository.findById(id).isEmpty()) {
+            return "Such id does not exist";
+        }
+        return callOneRepository.getReferenceById(id).getPhoneOne();
     }
 
     public String deletePhoneNumber() {
--- src/main/java/com/example/ymeqpvuunrfubnfukxtd/service/model_service/CallOneService.java
+++ src/main/java/com/example/ymeqpvuunrfubnfukxtd/service/model_service/CallOneService.java
@@ -7,4 +7,14 @@ public interface CallOneService {
     CallOne callOneDTOToCallOne(CallOneDTO callOneDTO);
 
     CallOneDTO callOneToCallOneDTO(CallOne callOne);
+
+    Boolean checkPhoneNumber(String number);
+
+    void addPhoneObject(CallOne callOne);
+
+    Boolean checkName(String name);
+
+    Boolean checkYear(Integer year);
+
+    Boolean checkDate(String date);
 }
